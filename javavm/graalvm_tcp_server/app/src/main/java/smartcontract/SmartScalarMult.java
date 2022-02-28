a5ab4eeab8d312689029c14f20985e7ac65deb0d
Jean Viaene <jean.viaene@epfl.ch>
2022-02-28 22:25:57 +0100

    can build scalar mult

--- javavm/graalvm_tcp_server/app/src/main/java/smartcontract/SmartScalarMult.java
+++ javavm/graalvm_tcp_server/app/src/main/java/smartcontract/SmartScalarMult.java
@@ -3,13 +3,44 @@ package smartcontract;
 public class SmartScalarMult implements SmartContract {
 
   public byte[] Execute(byte[] input) throws SmartContractException {
-    if (input.length != 32) {
+    final int ITERATIONS = 1000;
+
+    if (input.length != crypto_core_ed25519_SCALARBYTES) {
       throw new SmartContractException("failed to process: input should be 8 bytes long!");
     }
-  
-    // TODO: implement crypto scalar multiplication
-    byte[] output = "multiplied_value".getBytes();
+
+    // 8< -----------------------------
+    /* Read input message. A 32 bytes hex-encoded scalar */
+    // char input[crypto_core_ed25519_SCALARBYTES * 2];
+    // fgets(input, crypto_core_ed25519_SCALARBYTES * 2, f);
+
+    // unsigned char s[crypto_core_ed25519_SCALARBYTES];
+
+    // sodium_hex2bin(s, crypto_core_ed25519_SCALARBYTES,
+    // input, crypto_core_ed25519_SCALARBYTES * 2,
+    // NULL, NULL, NULL);
+
+    // unsigned char r[crypto_core_ed25519_BYTES];
+
+    // for (int i=0; i < ITERATIONS; i++) {
+    // crypto_scalarmult_ed25519_base_noclamp(r, s);
+    // }
+
+    // sodium_bin2hex(result, rlen, r, sizeof(r));
+    // ----------------------------- >8
+
+    byte[] buffer = new byte[crypto_core_ed25519_SCALARBYTES];
+
+    byte[] output = new byte[crypto_core_ed25519_SCALARBYTES];
+
+    SmartCrypto sc = new SmartCrypto();
+
+    for (int i = 0; i < ITERATIONS; i++) {
+      sc.scalarmult_ed25519_base_noclamp(output, input);
+    }
 
     return output;
   }
+
+  public final int crypto_core_ed25519_SCALARBYTES = 32;
 }
